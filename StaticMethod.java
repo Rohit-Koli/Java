57462c488cbba11325d7a325e370b3a319a8c6c0
Rohit Koli <85403005+Rohit-Koli@users.noreply.github.com>
2022-05-26 12:59:13 +0530

    Create StaticMethod.java

---
+++ StaticMethod.java
@@ -0,0 +1,12 @@
+class stat
+{
+    public static void Sample(){
+        System.out.println("It is Static Method !");
+    }
+}
+public class StaticMethod {
+    public static void main(String[] args) {
+        stat.Sample();
+    }
+}
+//Static Methods 
